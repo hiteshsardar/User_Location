4ef30470bf0d417565c7df3c00cc1162a9f704da
Hitesh Sardar <Hitesh.Sardar@progress.com>
2024-04-01 02:44:49 +0530

    first commit

---
+++ src/main/java/org/com/User_Location/User_Location/UserLocationApplication.java
@@ -0,0 +1,13 @@
+package org.com.User_Location.User_Location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/com/User_Location/User_Location/UserLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.User_Location.User_Location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
